21ca032999ec3304a2a12f7ade698328c258a6e6
permalik <permalik@protonmail.com>
2022-05-31 15:17:21 -0500

    Complete 1.6 course information

--- 11_course_info_step_one/solution_01/src/App.js
+++ 11_course_info_step_one/solution_01/src/App.js
@@ -3,28 +3,29 @@ import Header from './components/Header';
 import Total from './components/Total';
 
 const App = () => {
-  const course = 'Half stack application development';
-
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercise: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercise: 7
-    },
-    {
-      name: 'State of a component',
-      exercise: 14
-    }
-  ];
+  const course = {
+    name: 'Half stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercise: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercise: 7
+      },
+      {
+        name: 'State of a component',
+        exercise: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      <Header heading={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header heading={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
