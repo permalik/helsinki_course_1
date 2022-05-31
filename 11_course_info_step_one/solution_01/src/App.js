21413a86601186b5e6c27c590228923c8dd6e97f
permalik <permalik@protonmail.com>
2022-05-31 14:19:56 -0500

    Complete Content component

--- 11_course_info_step_one/solution_01/src/App.js
+++ 11_course_info_step_one/solution_01/src/App.js
@@ -1,26 +1,26 @@
+import Content from './components/Content';
 import Header from './components/Header';
 
 const App = () => {
   const course = 'Half stack application development';
   const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
   const part2 = 'Using props to pass data';
-  const exercises2 = 7;
   const part3 = 'State of a component';
+  const exercises1 = 10;
+  const exercises2 = 7;
   const exercises3 = 14;
 
   return (
     <div>
       <Header heading={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={part1}
+        exercises2={part2}
+        exercises3={part3}
+      />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
