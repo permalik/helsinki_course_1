c33cb85f33d1992822d250a568f7f3868cf79a6a
Malik <permalik@protonmail.com>
2022-06-26 09:49:58 -0500

    Complete 16_unicafe_step_one

--- 16_unicafe_step_one/src/App.js
+++ 16_unicafe_step_one/src/App.js
@@ -6,7 +6,51 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const incrementParam = (state, stateFunction) => {
+    stateFunction(state + 1);
+  };
+
+  let all = good + neutral + bad;
+
+  let average = all / 3;
+
+  let positive = all / good;
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <menu>
+        <button
+          onClick={() => {
+            incrementParam(good, setGood);
+          }}
+        >
+          good
+        </button>
+        <button
+          onClick={() => {
+            incrementParam(neutral, setNeutral);
+          }}
+        >
+          neutral
+        </button>
+        <button
+          onClick={() => {
+            incrementParam(bad, setBad);
+          }}
+        >
+          bad
+        </button>
+      </menu>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive ? `${positive}%` : 'unknown'}</p>
+    </div>
+  );
 };
 
 export default App;
