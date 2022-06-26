1fe69e4d4026838c6e903f0c8cd66550dc318b28
Malik <permalik@protonmail.com>
2022-06-26 10:02:36 -0500

    Complete 1.8 unicafe step 3

--- 16_unicafe_step_one/src/App.js
+++ 16_unicafe_step_one/src/App.js
@@ -1,5 +1,19 @@
 import { useState } from 'react';
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {props.all}</p>
+      <p>Average: {props.average}</p>
+      <p>Positive: {props.positive ? `${props.positive}%` : 'unknown'}</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -41,14 +55,14 @@ const App = () => {
         >
           bad
         </button>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          average={average}
+          positive={positive}
+        />
       </menu>
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive ? `${positive}%` : 'unknown'}</p>
     </div>
   );
 };
